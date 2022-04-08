3520f150235ea0a2d1afc956c3d34d10e201d925
xudan <875126243@qq.com>
2022-04-08 11:29:14 +0800

    demo

---
+++ demo/src/components/demo1.vue
@@ -0,0 +1,23 @@
+<template>
+    {{count}}
+    <button @click="updateCount">点击 </button>
+</template>
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+    setup() {
+        const count = ref(0)
+        console.log(count)
+
+        function updateCount(){
+             count.value++
+        }
+
+        return {
+            count,
+            updateCount
+        }
+    },
+})
+</script>
